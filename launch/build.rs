38d62d0669bac1236e917166de559b7ea224800a
lilydjwg <lilydjwg@gmail.com>
2018-01-10 10:49:21 +0800

    build.rs: use relative path

--- launch/build.rs
+++ launch/build.rs
@@ -1,4 +1,4 @@
 fn main() {
-  println!("cargo:rustc-link-search=native=/home/lilydjwg/workspace/remotebrowser");
+  println!("cargo:rustc-link-search=native=..");
   println!("cargo:rustc-link-lib=static=res");
 }
